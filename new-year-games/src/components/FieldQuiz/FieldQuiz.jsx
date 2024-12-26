53c8b21fc7f048c91a7e81f9cf3aca8a28a0c91e
Yana Tiulenina <122269490+YaTyulen@users.noreply.github.com>
2024-12-27 02:26:53 +0700

    добавление контента по игре квиз

--- new-year-games/src/components/FieldQuiz/FieldQuiz.jsx
+++ new-year-games/src/components/FieldQuiz/FieldQuiz.jsx
@@ -4,8 +4,15 @@ import category2 from '../../assets/image2.svg'
 import category3 from '../../assets/image3.svg'
 import category4 from '../../assets/image8.svg'
 import category5 from '../../assets/image9.svg'
+import back from '../../assets/back.svg'
+import { useNavigate } from 'react-router-dom';
 
 function FieldQuiz({selectQuestion, closedQuestions}) {
+    const navigate = useNavigate()
+
+    const clickBack = () => {
+        navigate('/field')
+    }
 
     const createCell = (categoryNum, questionNum) => {
         let jsx = [];
@@ -37,9 +44,10 @@ function FieldQuiz({selectQuestion, closedQuestions}) {
         return jsx;
     }
 
-    const createCol = (categoryNum) => {
+    const createCol = (categoryNum, name) => {
         let jsx = [];
 
+        jsx.push(<div className='quiz__col-name'>{name}</div>)
         for(let i = 1; i <= 5; i++) {
             jsx.push(createCell(categoryNum, i))
         }
@@ -50,17 +58,23 @@ function FieldQuiz({selectQuestion, closedQuestions}) {
     const createTable = () => {
         let jsx = [];
 
-        for(let i = 1; i <= 5; i++) {
-            jsx.push(<ul className='quiz__col'>{createCol(i)}</ul>)
-        }
+        jsx.push(<ul className='quiz__col'>{createCol(1, 'История')}</ul>)
+        jsx.push(<ul className='quiz__col'>{createCol(2, 'Фильмы')}</ul>)
+        jsx.push(<ul className='quiz__col'>{createCol(3, 'Верю-неверю')}</ul>)
+        jsx.push(<ul className='quiz__col'>{createCol(4, 'Песни')}</ul>)
+        jsx.push(<ul className='quiz__col'>{createCol(5, 'Загадки')}</ul>)
         
         return jsx;
     }
 
     return (
-        <div className='quiz__table'>
-            {createTable()}  
-        </div>
+        <>
+            <div className='quiz__table'>
+                {createTable()}  
+            </div>
+            <div className='quiz__back' onClick={() => clickBack()}><img src={back} width='50px' height='50px'/></div>
+        </>
+        
     );
 }
 
--- new-year-games/src/components/Question/Question.jsx
+++ new-year-games/src/components/Question/Question.jsx
@@ -1,9 +1,82 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import './Question.scss';
 import Button from '../UI/Button/Button';
+import question1 from '../../media/audio/quiz/question1_quiz.mp3'
+import response2 from '../../media/audio/quiz/response2_quiz.mp3'
+import response16 from '../../media/audio/response8.mp3'
+import response17 from '../../media/audio/quiz/response17_quiz.mp3'
+import response18 from '../../media/audio/quiz/response18_quiz.mp3'
+import response19 from '../../media/audio/quiz/response19_quiz.mp3'
+import response20 from '../../media/audio/quiz/response20_quiz.mp3'
+import cadr1 from '../../assets/cadr1.png'
+import cadr2 from '../../assets/cadr2.png'
+import cadr3 from '../../assets/cadr3.png'
+import question19 from '../../media/audio/quiz/question19_quiz.mp3'
+import question10 from '../../media/audio/quiz/question10_quiz.mp3'
+import question10Clue1 from '../../media/audio/quiz/question10_quiz_clue1.mp3'
+import question10Clue2 from '../../media/audio/quiz/question10_quiz_clue2.mp3'
+import audioThirdWheel1 from '../../media/audio/quiz/audioThirdWheel1.mp3'
+import audioThirdWheel2 from '../../media/audio/quiz/audioThirdWheel2.mp3'
+import audioThirdWheel3 from '../../media/audio/quiz/audioThirdWheel3.mp3'
+import audioThirdWheel4 from '../../media/audio/quiz/audioThirdWheel4.mp3'
 
 function Question({question, clickBack}) {
     const [isResponse, setResponse] = useState(false)
+    const [img, setImg] = useState(cadr1)
+    const [audio, setAudio] = useState()
+    const [audioResponse, setAudioResponse] = useState()
+    const [clue, setClue] = useState(0)
+    
+    useEffect(() => {
+
+        switch (question.num) {
+            case 6:
+                setAudio(question1)
+                break;
+            case 7:
+                setAudioResponse(response2)
+                break;
+            case 10:
+                setAudio(question10)
+                break;
+            case 16:
+                setAudioResponse(response16)
+                break;
+            case 17:
+                setAudioResponse(response17)
+                break;
+            case 18:
+                setAudioResponse(response18)
+                break;
+            case 19:
+                setAudio(question19)
+                setAudioResponse(response19)
+                break;
+            case 20:
+                setAudioResponse(response20)
+                break;
+            default:
+                break;
+        }
+
+        switch (clue) {
+            case 1:
+                setImg(cadr2)
+                if(question.num === 10){
+                    setAudio(question10Clue1)
+                }
+                break;
+            case 2:
+                setImg(cadr3)
+                if(question.num === 10){
+                    setAudio(question10Clue2)
+                }
+                break;
+            default:
+                break;
+        }
+    }, [clue, question.num])
+  
   
     return (
         <div className='cart-question cart-question__wrapper'>
@@ -11,14 +84,49 @@ function Question({question, clickBack}) {
                 <div className='cart-question__name'>Вопрос {question.num}</div> 
 
                 {isResponse 
-                    ? <div className='cart-question__wrapper-response'>
-                        <div className='cart-question__response'>Ответ</div>
-                    </div> 
-                : <div className='cart-question__text'>Вопрос</div>}
+                    ? <>
+                        <div className='cart-question__wrapper-response'>
+                            <div className='cart-question__response'>{question.response}</div>
+                        </div> 
+                        {question.response_text && <div className='cart-question__wrapper-response'>
+                            <div className='cart-question__response'>{question.response_text}</div>
+                        </div>}
+                        {question.audio_response && <div className='cart-question__audio'>
+                            <iframe hidden src={audioResponse} allow="autoplay" type="audio/mpeg"/>
+                        </div>}
+                    </>
+                : <div className='cart-question__question-wrapper'>
+                    {question.task && <div className='cart-question__text'>{question.task}</div>}
+                    <div className='cart-question__text'>{question.question}</div>
+                    {question.audio_question && clue === 0 && <div className='cart-question__audio'>
+                        <audio src={audio} controls autoplay></audio> 
+                    </div> }
+                    {question.audio_question && clue > 0 && question.clue && <div className='cart-question__audio'>
+                        <audio src={audio} controls autoplay></audio> 
+                    </div> }
+                    {question.third_wheel && <div className='cart-question__audio'>
+                        <audio src={audioThirdWheel1} controls autoplay></audio> 
+                        <audio src={audioThirdWheel2} controls autoplay></audio> 
+                        <audio src={audioThirdWheel3} controls autoplay></audio> 
+                        <audio src={audioThirdWheel4} controls autoplay></audio> 
+                    </div> }
+                    {question.image && <div className='cart-question__img'>
+                        <img className='cadr' src={img} width='550px'/>
+                    </div> }
+                    {question.options && <div className='cart-question__options'>
+                        <ul className='quiz__options'>
+                            <li className='quiz__option'>{question.option1}</li>
+                            <li className='quiz__option'>{question.option2}</li>
+                            <li className='quiz__option'>{question.option3}</li>
+                            <li className='quiz__option'>{question.option4}</li>
+                        </ul>
+                    </div> }
+                </div>}
 
                 <div className='cart-question__button-container'>
+                    {question.clue && <Button text='Подсказка' onClick={()=> {setClue(clue+1)}}/>}
                     {isResponse 
-                        ? <Button text='Назад' onClick={() => {setResponse(false); clickBack()}}/> 
+                        ? <Button text='Назад' onClick={() => {setResponse(false); setClue(0); clickBack()}}/> 
                         : <Button text='Ответ' onClick={()=> {setResponse(true)}}/>}  
                 </div>
             </div>
